cb02e0eeb0bbdb665799828d92e87c4ef76b49d9
Daniel Goldberg <danpaulgo@aol.com>
2017-02-08 23:02:53 -0500

    Directory recreated using 'bundle gem' command

---
+++ lib/s_and_p_analyzer.rb
@@ -0,0 +1,5 @@
+require "s_and_p_analyzer/version"
+
+module SAndPAnalyzer
+  # Your code goes here...
+end
---
+++ spec/s_and_p_analyzer_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe SAndPAnalyzer do
+  it "has a version number" do
+    expect(SAndPAnalyzer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
